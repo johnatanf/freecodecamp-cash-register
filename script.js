6c8048e8ddc17881b73648c96f8550cb5982ceea
johnatanf <johnatan.f100@gmail.com>
2024-06-14 09:25:05 +0700

    add cashAmounts array and getLargestAvailableCashAmountIndex function

--- script.js
+++ script.js
@@ -11,10 +11,32 @@ let cid = [
   ["ONE HUNDRED", 100]
 ];
 
+let cashAmounts = [
+    ["PENNY", 0.01],
+    ["NICKEL", 0.05],
+    ["DIME", 0.1],
+    ["QUARTER", 0.25],
+    ["ONE", 1],
+    ["FIVE", 5],
+    ["TEN", 10],
+    ["TWENTY", 20],
+    ["ONE HUNDRED", 100]
+];
+
 function getTotalCidAmount(cid) {
     let total = 0
     for(let arr of cid) {
         total += arr[1] // index 1, the total amount of a cash amount
     }
     return Number(total.toFixed(2))
+}
+
+function getLargestAvailableCashAmountIndex(change, cashAmounts, cid) {    
+    for(let i = cashAmounts.length - 1; i > 0; i--) { //start from highest index
+        if(cashAmounts[i][1] <= change && cid[i][1] > 0) { // if a current cash amount is smaller than or equal to change, and is available in cid
+            return i
+        }
+    }
+
+    return null // in case smallest amount i.e. pennies are not available
 }
