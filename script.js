491e4c6ba962d09c24069e37516678933195fdeb
johnatanf <johnatan.f100@gmail.com>
2024-06-14 10:11:43 +0700

    add getChangeString function

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 let price = 1.87;
+// let cid = [
+//   ["PENNY", 1.01],
+//   ["NICKEL", 2.05],
+//   ["DIME", 3.1],
+//   ["QUARTER", 4.25],
+//   ["ONE", 90],
+//   ["FIVE", 55],
+//   ["TEN", 20],
+//   ["TWENTY", 60],
+//   ["ONE HUNDRED", 100]
+// ];
+
 let cid = [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100]
-];
+    ["PENNY", 0.04],
+    ["NICKEL", 0.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100]
+  ];
 
 let cashAmounts = [
     ["PENNY", 0.01],
@@ -39,4 +51,39 @@ function getLargestAvailableCashAmountIndex(change, cashAmounts, cid) {
     }
 
     return null // in case smallest amount i.e. pennies are not available
+}
+
+function getChangeString(change, cid) {
+    let remainingChange = change
+    let finalChange = [
+        ["PENNY", 0],
+        ["NICKEL", 0],
+        ["DIME", 0],
+        ["QUARTER", 0],
+        ["ONE", 0],
+        ["FIVE", 0],
+        ["TEN", 0],
+        ["TWENTY", 0],
+        ["ONE HUNDRED", 0]
+    ]
+    let finalChangeString = ""
+    let currentCashAmount = null
+    let currentCashAmountIndex = null
+
+    while(remainingChange > 0) {
+        currentCashAmountIndex = getLargestAvailableCashAmountIndex(remainingChange, cashAmounts, cid)
+
+        if(currentCashAmountIndex !== null) {
+            currentCashAmount = cashAmounts[currentCashAmountIndex][1]
+
+            remainingChange = Number(parseFloat(remainingChange - currentCashAmount).toFixed(2))
+            cid[currentCashAmountIndex][1] = Number(parseFloat(cid[currentCashAmountIndex][1] - currentCashAmount).toFixed(2))
+            finalChange[currentCashAmountIndex][1] = Number(parseFloat(finalChange[currentCashAmountIndex][1] + currentCashAmount).toFixed(2))
+
+        } else {
+            return "" // cannot make exact change
+        }
+    }
+    finalChangeString = finalChange
+    return finalChangeString
 }
