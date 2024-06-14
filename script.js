93ae71d0d6be6be7c8ce37e3a2e6ff23e783bee4
johnatanf <johnatan.f100@gmail.com>
2024-06-14 10:12:16 +0700

    fix getChangeString

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 let price = 1.87;
-// let cid = [
-//   ["PENNY", 1.01],
-//   ["NICKEL", 2.05],
-//   ["DIME", 3.1],
-//   ["QUARTER", 4.25],
-//   ["ONE", 90],
-//   ["FIVE", 55],
-//   ["TEN", 20],
-//   ["TWENTY", 60],
-//   ["ONE HUNDRED", 100]
-// ];
-
 let cid = [
-    ["PENNY", 0.04],
-    ["NICKEL", 0.05],
-    ["DIME", 3.1],
-    ["QUARTER", 4.25],
-    ["ONE", 90],
-    ["FIVE", 55],
-    ["TEN", 20],
-    ["TWENTY", 60],
-    ["ONE HUNDRED", 100]
-  ];
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100]
+];
 
 let cashAmounts = [
     ["PENNY", 0.01],
